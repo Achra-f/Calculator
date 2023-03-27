117772e50cf331ec46788879ab399179dfcc5055
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-03-28 01:50:47 +0200

    display numbers

---
+++ script.js
@@ -0,0 +1,92 @@
+//input variables
+let firstOperand = 0;
+let secondOperand = 0;
+let currentOperator = null;
+
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-operator]');
+const clear = document.getElementById('clear');
+const deleteScreen = document.getElementById('delete');
+const equals = document.getElementById('equals');
+const point = document.getElementById('point');
+const result = document.getElementById('result');
+
+//math functions
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+//operator function
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+            return add(a, b)
+        case '−':
+            return subtract(a, b)
+        case '×':
+            return multiply(a, b)
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
+    }
+
+}
+
+//show numbers
+numbers.forEach((button) => {
+    button.addEventListener('click', () => changeNumber(button.textContent));
+});
+
+function changeNumber(number) {
+    const screenNumber = result.innerHTML;
+    if (screenNumber.length < 9)
+        result.textContent = parseInt(screenNumber + number).toString();
+}
+
+
+//select operator
+operators.forEach((button) => {
+    button.addEventListener('click', () => changeOperator(button.textContent));
+})
+
+function changeOperator(operator) {
+    if (operator == '+') {
+        currentOperator = "+";
+    }
+    else if (operator == '-') {
+        currentOperator = "-";
+    }
+    else if (operator == '×') {
+        currentOperator = "×";
+    }
+    else if (operator == '÷') {
+        currentOperator = "÷";
+    }
+}
+
+//set operation
+function setOperation(operator) {
+    firstOperand = result.textContent
+    currentOperation = operator
+
+
+}
+
+
+
